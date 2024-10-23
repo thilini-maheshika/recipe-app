869930fbb012bdc8a8bbac7028ee29966844cd72
thilinimaheshika <kthilini1999@gmail.com>
2024-10-23 12:58:18 +0530

    added application header to frontend

--- recipe-backend/index.js
+++ recipe-backend/index.js
@@ -21,6 +21,7 @@ app.use(function (req, res, next) {
         res.header('Access-Control-Allow-Origin', origin);
     }
 
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-token');
 
